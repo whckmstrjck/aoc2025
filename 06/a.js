a722f002c7988647ff3b0c7b85e8e368a0eb8e6f
Tobias Mattsson <tobias.mattsson@gmail.com>
2025-12-06 12:51:30 +0100

    Solves 06a

--- 06/a.js
+++ 06/a.js
@@ -1,2 +1,39 @@
 const fs = require('fs');
-const input = fs.readFileSync('test-input.txt', 'utf8');
+const input = fs.readFileSync('input.txt', 'utf8');
+const inputGrid = input.split('\n').map((line) => line.trim().replace(/\s+/g, ' ').split(' '));
+
+let done = false;
+let column = 0;
+const problems = [];
+
+while (!done) {
+  for (let row = 0; row < inputGrid.length; row++) {
+    if (inputGrid[row][column] === undefined) {
+      done = true;
+      break;
+    }
+
+    problems[column] = problems[column] ?? [];
+    problems[column].push(inputGrid[row][column]);
+  }
+  column++;
+}
+
+const grandTotal = problems.reduce((acc, problem) => {
+  const operator = problem.pop();
+
+  return (
+    problem.reduce(
+      (total, numberStr) => {
+        if (operator === '+') {
+          return total + Number(numberStr);
+        } else {
+          return total * Number(numberStr);
+        }
+      },
+      operator === '*' ? 1 : 0
+    ) + acc
+  );
+}, 0);
+
+console.log('🧮 GRAND TOTAL:', grandTotal);
