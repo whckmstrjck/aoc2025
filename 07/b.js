801b1f234400d6ef8cc9c21a6ce713fff024f659
Tobias Mattsson <tobias.mattsson@gmail.com>
2025-12-07 20:55:48 +0100

    Solves 07b

--- 07/b.js
+++ 07/b.js
@@ -1,20 +1,32 @@
 const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8').split('\n');
 
-let beamSplits = 0;
-const beamIndexes = new Set();
-beamIndexes.add(input[0].indexOf('S'));
+const result = input.map((row) => row.split('').map(() => 0));
 
-for (let y = 1; y < input.length; y++) {
-  for (let x = 0; x < input[y].length; x++) {
-    const cell = input[y][x];
-    if (cell === '^' && beamIndexes.has(x)) {
-      beamSplits++;
-      beamIndexes.delete(x);
-      beamIndexes.add(x - 1);
-      beamIndexes.add(x + 1);
+for (let y = 0; y < input.length; y++) {
+  const line = input[y];
+
+  for (let x = 0; x < line.length; x++) {
+    const currentChar = line[x];
+
+    if (currentChar === 'S') {
+      result[y][x] = 1;
+      continue;
     }
+
+    if (currentChar === '^') {
+      result[y][x - 1] += result[y - 1][x];
+      result[y][x] = '^';
+      result[y][x + 1] += result[y - 1][x];
+      continue;
+    }
+
+    if (!result[y - 1] || result[y - 1]?.[x] === '^') continue;
+
+    result[y][x] += result[y - 1][x];
   }
 }
 
-console.log('⚡️ BEAM SPLITS:', beamSplits);
+const possibleTimelines = result.at(-1).reduce((sum, timelineCount) => sum + timelineCount, 0);
+
+console.log('🕰️ POSSIBLE TIMELINES:', possibleTimelines);
